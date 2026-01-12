6daa23bc991e706f96139768606d1e8fc261bff7
Oliv97432 <inbyoliver@gmail.com>
2026-01-12 08:03:55 +0400

    Update vite.config.mjs

--- woofly/vite.config.mjs
+++ woofly/vite.config.mjs
@@ -17,13 +17,7 @@ export default defineConfig({
     }
   },
   build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
-      }
-    },
+    minify: 'esbuild',  // ✅ CHANGÉ ICI : esbuild au lieu de terser
     chunkSizeWarningLimit: 500,
     sourcemap: false,
     cssCodeSplit: true,
@@ -57,3 +51,4 @@ export default defineConfig({
     host: true
   }
 });
+
